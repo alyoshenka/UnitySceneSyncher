4c288b15d765973163c549ee19e1634ad4a37798
Alexi Most <alexibmost@gmail.com>
2019-11-06 16:29:20 -0800

    proj files

---
+++ Assets/Scripts/CaptureSceneView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class CaptureSceneView : MonoBehaviour
+{
+
+    void Update()
+    {
+        Debug.Log(SceneView.lastActiveSceneView.position);
+    }
+    void OnGUI()
+    {
+        Debug.Log(SceneView.lastActiveSceneView.position);
+    }
+}
