83b704755132d0fc06ab32f9a13c89bf488c3fa3
Alexi Most <alexibmost@gmail.com>
2019-11-06 16:49:24 -0800

    scene camera pos

--- Assets/Scripts/CaptureSceneView.cs
+++ Assets/Scripts/CaptureSceneView.cs
@@ -6,13 +6,14 @@ using UnityEditor;
 [ExecuteInEditMode]
 public class CaptureSceneView : MonoBehaviour
 {
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(255, 0, 200, 1);
+        Gizmos.DrawSphere(SceneView.lastActiveSceneView.camera.transform.position, 1);
 
-    void Update()
-    {
-        Debug.Log(SceneView.lastActiveSceneView.position);
-    }
-    void OnGUI()
-    {
-        Debug.Log(SceneView.lastActiveSceneView.position);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(SceneView.lastActiveSceneView.camera.transform.position, 
+                        SceneView.lastActiveSceneView.camera.transform.position 
+                        + SceneView.lastActiveSceneView.camera.transform.rotation * Vector3.forward * 5);
     }
 }
