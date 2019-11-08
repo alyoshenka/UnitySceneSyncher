e0ffda061e65450d8c49ab9b6d2b656e93c6d798
Alexi Most <alexibmost@gmail.com>
2019-11-07 16:38:52 -0800

    upgrade unity verz

--- Assets/Scripts/GameDev.cs
+++ Assets/Scripts/GameDev.cs
@@ -1,5 +1,19 @@
-﻿
+﻿using UnityEngine;
+
 struct GameDev
 {
+    // user configurable variables
+    string name;
 
+    // extra variables
+    Color displayColor;
+    Sprite displayIcon;
+
+    // constant variables
+    Vector3 position;
+    Quaternion rotation;
+    GameObject currentlySelectedObject;
 }
+
+// ability to "lock" item in hierarchy
+    // or change its color to indicate it shouldn't be moved by anyone else
