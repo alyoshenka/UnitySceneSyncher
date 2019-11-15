545465551ebe926f6478ab87d12c96d91fa71964
Alexi Most <alexibmost@gmail.com>
2019-11-14 16:13:14 -0800

    LiteNetLib integration

---
+++ ServerTest/Client/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using LiteNetLib;
+using System.Threading;
+
+namespace LiteNetLibTest
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager client = new NetManager(listener);
+            client.Start();
+            client.Connect("localhost", 9050, "Key");
+            listener.NetworkReceiveEvent += (fromPeer, dataReader, deliveryMethod) =>
+            {
+                Console.WriteLine("We got {0}", dataReader.GetString(100));
+                dataReader.Recycle();
+            };
+
+            while (!Console.KeyAvailable)
+            {
+                client.PollEvents();
+                Thread.Sleep(15);
+            }
+
+            client.Stop();
+        }
+    }
+}
+
---
+++ ServerTest/Server/Server.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using LiteNetLib;
+using System.Threading;
+using LiteNetLib.Utils;
+
+namespace LiteNetLibTest
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager server = new NetManager(listener);
+            server.Start(9050);
+
+            listener.ConnectionRequestEvent += request =>
+            {
+                if (server.PeersCount < 10) { request.AcceptIfKey("Key"); }
+                else { request.Reject(); }
+            };
+
+            listener.PeerConnectedEvent += peer =>
+            {
+                Console.WriteLine("We got connection: {0}", peer.EndPoint);
+                NetDataWriter writer = new NetDataWriter();
+                writer.Put("Hello client!");
+                peer.Send(writer, DeliveryMethod.ReliableOrdered);
+            };
+
+            while (!Console.KeyAvailable)
+            {
+                server.PollEvents();
+                Thread.Sleep(15);
+            }
+
+            server.Stop();
+        }
+    }
+}
