375fe4db5cdec42ae8ddf971e08ac77b1acd95f8
alexi <alexibmost@gmail.com>
2019-11-26 23:14:54 -0800

    networking in unity

---
+++ Assets/Scripts/Network/Client.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using System.Threading;
+
+public class Client : MonoBehaviour
+{
+    const int listenPort = 11000;
+
+    UdpClient listener;
+    IPEndPoint groupEP;
+
+    Thread worker;
+
+    private void Start()
+    {
+        listener = new UdpClient(listenPort);
+        groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+        worker = new Thread(Go);
+        worker.Start();
+
+        Debug.Log("listener started");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) { worker.Abort(); }  
+    }
+
+    void Go()
+    {
+        try
+        {
+            while (true)
+            {
+                Debug.Log("Waiting for broadcast");
+                byte[] bytes = listener.Receive(ref groupEP);
+
+                Debug.Log("Recieved from " + groupEP);
+                Debug.Log("Message: " + Encoding.ASCII.GetString(bytes, 0, bytes.Length));
+            }
+        }
+        catch (SocketException e) { Debug.LogError(e); }
+        finally { listener.Close(); }
+
+        Debug.Log("listener closed");
+    }
+
+    void Boop()
+    {
+        while (true) { Debug.Log("boop"); }
+    }
+}
---
+++ Assets/Scripts/Network/Server.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class Server : MonoBehaviour
+{
+    Socket s;
+    IPAddress broadcast;
+
+    void Start()
+    {
+        s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        broadcast = IPAddress.Parse("192.168.0.4");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            byte[] sendbuf = Encoding.ASCII.GetBytes("boop");
+            IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+            s.SendTo(sendbuf, ep);
+
+            Debug.Log("message sent");
+        }
+    }
+}
