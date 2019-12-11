a4a7c74b5d0c377f7bd8aabd6794e40d57488809
alexi <alexibmost@gmail.com>
2019-12-11 12:41:31 -0800

    added server

---
+++ EditorWatcher/EditorWatcher/Program.cs
@@ -0,0 +1,18 @@
+﻿#define UNITY_EDITOR
+
+using System;
+
+namespace EditorWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("boop");
+#if UNITY_ENGINE_AVAILABLE
+                       Console.WriteLine(EditorWindow.focusedWindow);
+#endif
+            Console.ReadLine();
+        }
+    }
+}
