d4e7c0ed412ffea50913c2bc3006818126c95fb9
Alexi Most <alexibmost@gmail.com>
2019-12-18 17:34:39 -0800

    displaying connected developers

---
+++ Assets/Editor/PeerDisplay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PeerDisplayWindow : EditorWindow
+{
+    Client client;
+
+    [MenuItem("Window/UnitySceneSyncher/PeerDisplay")]
+    static void Init()
+    {
+        PeerDisplayWindow win = (PeerDisplayWindow)GetWindow(typeof(PeerDisplayWindow));
+        win.Show();
+    }  
+
+    private void Awake()
+    {
+       client = ((ClientWindow)GetWindow(typeof(ClientWindow), false, null, false)).client;
+    }
+
+    private void OnGUI()
+    {
+        if(null == client) { client = ((ClientWindow)GetWindow(typeof(ClientWindow), false, null, false)).client; }
+        if(null == client) { return; }
+
+        foreach(Network.Developer dev in client.developers)
+        {
+            GUILayout.Label(dev.GetName());
+        }
+    }
+}
